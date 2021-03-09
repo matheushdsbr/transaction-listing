ac9791f746c1e86bd1538c69111771a4cca88dd1
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-03-09 12:30:19 -0300

    style: input float label

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,3 +1,42 @@
 import styled from 'styled-components';
 
-export const InputDefault = styled.input``;
+export const InputContainer = styled.div`
+  position: relative;
+  width: 100%;
+
+  input {
+    border-radius: 12px;
+    border: 1px solid blue;
+    color: #454550;
+    height: 12px;
+    outline: none;
+    padding: 18px;
+    transition: all 0.2s ease-out;
+    width: 100%;
+
+    &:focus {
+      border: 1px solid green;
+    }
+
+    &:focus ~ label,
+    &:valid ~ label {
+      color: green;
+      padding: 0px 8px;
+      top: -20px;
+    }
+  }
+
+  label {
+    background: white;
+    border-radius: 12px;
+    color: blue;
+    cursor: text;
+    left: 0;
+    margin-left: 15px;
+    margin-top: 7px;
+    padding: 0px 8px;
+    position: absolute;
+    top: 0;
+    transition: all 0.2s ease-out;
+  }
+`;

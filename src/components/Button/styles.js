7fd91cf0aa691925d7cb7e365238073bfbec5699
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-03-09 12:35:49 -0300

    feat: add button styles default

---
+++ src/components/Button/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ButtonField = styled.button`
+  border: none;
+  background: blue;
+  border-radius: 5px;
+  width: 100%;
+  color: #fff;
+  padding: 8px 0;
+`;

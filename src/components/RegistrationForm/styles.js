f0ee3dda2b02071b68928b5555cbe720c01f8883
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-03-10 08:32:42 -0300

    refactor: remove container styles

--- src/components/RegistrationForm/styles.js
+++ src/components/RegistrationForm/styles.js
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
-
 export const TextError = styled.p`
   color: ${(props) => (props.error && 'red') || (props.success && 'green')};
 `;

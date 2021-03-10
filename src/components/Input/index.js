abb79f8a7d6939c9f1fe034092e4e2e4d3846fb3
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-03-09 23:30:09 -0300

    feat: add error handling

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { InputContainer } from './styles';
 
-const Input = ({ name, onChange, value }) => (
+const Input = ({ name, onChange, value, error }) => (
   <>
-    <InputContainer>
+    <InputContainer error={error}>
       <input type="text" id={name} value={value} onChange={onChange} required />
       <label htmlFor={name}>{name}</label>
     </InputContainer>

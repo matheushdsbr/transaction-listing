72009213fb6b6289388f7406c64d4777aae91123
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-03-10 08:29:51 -0300

    feat: add props search to icon-search

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { InputContainer } from './styles';
 
-const Input = ({ name, onChange, value, error }) => (
+const Input = ({ name, onChange, value, error, search }) => (
   <>
-    <InputContainer error={error}>
+    <InputContainer error={error} search={search}>
       <input type="text" id={name} value={value} onChange={onChange} required />
       <label htmlFor={name}>{name}</label>
     </InputContainer>

571dbea90ea2178848e3c0f430a4f18e9e0d6892
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-03-10 08:27:48 -0300

    style: add image default styles

--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -3,4 +3,11 @@ import styled from 'styled-components';
 export const FormField = styled.form`
   position: relative;
   width: 100%;
+
+  img {
+    position: absolute;
+    top: 50%;
+    right: 25px;
+    transform: translateY(-50%);
+  }
 `;

3d110baaf493901c17e584dbcd658f0877a30670
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-03-11 08:32:00 -0300

    style: add containertitle, containerlist default styles

--- src/components/SearchForm/styles.js
+++ src/components/SearchForm/styles.js
@@ -1,8 +1,34 @@
 import styled from 'styled-components';
+import colors from '../../tokens/colors';
 
 export const Container = styled.div`
   width: 100%;
   margin-bottom: 40px;
 `;
 
-export const FormContainer = styled.div``;
+export const ContainerTitle = styled.div`
+  text-align: center;
+
+  p {
+    font-size: 20px;
+
+    strong {
+      color: ${colors.primary};
+    }
+  }
+`;
+
+export const ContainerList = styled.div`
+  margin-top: 40px;
+  border: 1px solid ${colors.primary};
+  border-radius: 5px;
+  padding: 10px 20px;
+
+  p {
+    margin-bottom: 10px;
+
+    strong {
+      color: ${colors.primary};
+    }
+  }
+`;
